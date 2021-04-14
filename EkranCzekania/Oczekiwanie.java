9ad321f51c5fec97a4d9104657d8194da33f5f15
rudnikk <krzysiekrudnik7@gmail.com>
2021-04-14 19:11:55 +0200

    Dodanie okna czekania na połączenie

---
+++ EkranCzekania/Oczekiwanie.java
@@ -0,0 +1,38 @@
+package EkranCzekania;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class Oczekiwanie{
+    private static JFrame frame = new JFrame();
+    private static Icon gif;
+    private static JLabel g;
+    public Oczekiwanie(){
+        gif = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("lad.gif")));
+
+        g = new JLabel(gif);
+        frame.getContentPane().add(g);
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(40, 40, 20, 40));
+        panel.setLayout((new GridLayout(0, 1)));
+        panel.add(g);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+        frame.setTitle("Oczekiwanie na połączenie...");
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public void pokaz_ekran_czekania(){
+        new Oczekiwanie();
+    }
+    public void przestan_pokazywac_ekran_czekania(){
+        Oczekiwanie.frame.dispose();
+    }
+    public static void main(String[] args){  //to tylko test tego nie będzie
+        new Oczekiwanie(); // wywoływanie okna
+        //Oczekiwanie.frame.dispose(); //zamykanie okna
+    }
+}
