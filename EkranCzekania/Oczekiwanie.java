9a949bac81607578b7175162ac8ebaa08aa10dcf
rudnikk <krzysiekrudnik7@gmail.com>
2021-04-15 10:40:02 +0200

    v0.0.4 Drobne poprawki

--- EkranCzekania/Oczekiwanie.java
+++ EkranCzekania/Oczekiwanie.java
@@ -2,32 +2,31 @@ package EkranCzekania;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Objects;
-
-public class Oczekiwanie extends JFrame{
-    private static JFrame frame = new JFrame();
-    private static Icon gif;
-    private static JLabel g;
-    public Oczekiwanie(){
+public class OknoOczekiwania extends JFrame{
+    private Icon gif;
+    private JLabel g;
+    public OknoOczekiwania(){
         gif = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("lad.gif")));
         g = new JLabel(gif);
-        frame.getContentPane().add(g);
+        getContentPane().add(g);
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(40, 40, 20, 40));
         panel.setLayout((new GridLayout(0, 1)));
         panel.add(g);
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
-        frame.setTitle("Oczekiwanie na połączenie...");
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        add(panel, BorderLayout.CENTER);
+        setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Oczekiwanie na połączenie...");
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
-    public void pokaz_ekran_czekania(){
-        new Oczekiwanie();
+    public static OknoOczekiwania pokaz_ekran_czekania(){
+        OknoOczekiwania ocz = new OknoOczekiwania();
+        return ocz;
     }
     public void przestan_pokazywac_ekran_czekania(){
-        Oczekiwanie.frame.dispose();
+        dispose();
     }
+
 }
