f5edd2605e72c1eb9dda18323ec7483da417d9f8
rudnikk <krzysiekrudnik7@gmail.com>
2021-04-28 09:48:53 +0200

    v0.0.8 Usunięcie niepotrzebnych metod i pól oraz dodanie pola idpola i metody getIdPola()

--- Pawn.java
+++ Pawn.java
@@ -1,31 +1,21 @@
 
+
 public class Pawn {
 
     private String color;
     private String pawnID;
-    private int pawnPosition;
-
-    public Pawn(String color, String pawnID, int pawnPosition){
+    public String idpola;
+    public Pawn(String color, String pawnID, String idpola){
         this.color = color;
         this.pawnID = pawnID;
-        this.pawnPosition = pawnPosition;
+        this.idpola = idpola;
     }
 
     public String getPawnID(){
         return pawnID;
     }
-
-    public int getPosition(){
-        return pawnPosition;
-    }
-
     public String getPawnColor(){
         return color;
     }
-
-    public void killPawn(){
-        pawnPosition = -1;
-    }
-
-
+    public String getIdPola(){ return idpola; }
 }
